3731a006e86ff667ee087acf933c8c41140d9701
elonbot <102503134+ElonWang8@users.noreply.github.com>
2022-06-07 00:54:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'pianobene.elonbot.top:8443'
+      url.hostname = 'pianobene.elonbot.top'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
